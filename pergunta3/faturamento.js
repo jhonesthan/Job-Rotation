612ae4ae67c32fdaf9e0bcad14883c7c310bca3d
Jhone Sthan <60678458+jhonesthan@users.noreply.github.com>
2023-03-03 05:05:37 -0400

    Add files via upload

---
+++ pergunta3/faturamento.js
@@ -0,0 +1,26 @@
+
+
+const faturamentoMensal = [
+    22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 
+    26742.6612, 0.0, 42889.2258, 46251.174, 11191.4722, 0.0, 
+    0.0, 3847.4823, 373.7838, 2659.7563, 48924.2448, 18419.2614, 0.0, 
+    0.0, 35240.1826, 43829.1667, 18235.6852, 4355.0662, 13327.1025, 0.0, 
+    0.0, 25681.8318, 1718.1221, 13220.495, 8414.61];
+
+const menorFaturamento = Math.min(...faturamentoMensal);
+
+const maiorFaturamento = Math.max(...faturamentoMensal);
+
+const mediaFaturamento = faturamentoMensal.reduce((total, valor) => total + valor, 0) / faturamentoMensal.length;
+
+
+let diasAcimaDaMedia = 0;
+    faturamentoMensal.forEach((valor) => {
+        if (valor > mediaFaturamento) {
+        diasAcimaDaMedia++;
+    }
+    });
+
+console.log(`O menor valor de faturamento ocorrido em um dia do mês: ${menorFaturamento}`);
+console.log(`O maior valor de faturamento ocorrido em um dia do mês: ${maiorFaturamento}`);
+console.log(`Número de dias com faturamento acima da média mensal: ${diasAcimaDaMedia} `);
